ffd562780b4c488fe28349bc7662ff4c1cc72729
root <root@ldc-prd-matrix-001.openmarket.com>
2016-04-06 16:04:29 +0100

    Add vector.im resources to git
    Import the existing vector.im bits

---
+++ download/install.plist.php
@@ -0,0 +1,52 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; echo "\n"; ?>
+<?php echo '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">'; echo "\n"; ?>
+<?php
+
+require_once('fetchbuild.inc.php');
+
+$stream = 'master';
+if (isset($_GET['dev'])) {
+    $stream = 'develop';
+}
+
+$buildInfo = fetch_ios_build($stream);
+extract($buildInfo);
+
+?>
+<plist version="1.0">
+<dict>
+	<key>items</key>
+	<array>
+		<dict>
+			<key>assets</key>
+			<array>
+				<dict>
+					<key>kind</key>
+					<string>software-package</string>
+					<key>url</key>
+				<string>https://vector.im/download/<?php echo $ipaPath; ?></string>
+				</dict>
+				<dict>
+					<key>kind</key>
+					<string>display-image</string>
+					<key>needs-shine</key>
+					<false/>
+					<key>url</key>
+					<string>https://vector.im/download/<?php echo $iconPath; ?></string>
+				</dict>
+			</array>
+			<key>metadata</key>
+			<dict>
+				<key>bundle-identifier</key>
+                <string><?php echo $bundleId; ?></string>
+				<key>bundle-version</key>
+				<string><?php echo $bundleVersion; ?></string>
+				<key>kind</key>
+				<string>software</string>
+				<key>title</key>
+                <string>Vector, <?php echo $stream; ?> integration <?php echo $build; ?></string>
+			</dict>
+		</dict>
+	</array>
+</dict>
+</plist>
