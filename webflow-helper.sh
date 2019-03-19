a713e126e08c29f62face7b1641ca3e809e6a9b2
Nad Chishtie <n.chishtie@gmail.com>
2019-03-19 13:52:41 +0100

    Added script to clean up webflow export.

---
+++ webflow-helper.sh
@@ -0,0 +1,7 @@
+perl -pi -w -e 's#href="index.html"#href="/"#g;' *.html &&
+perl -pi -w -e 's#href="careers.html"#href="/careers"#g;' *.html &&
+perl -pi -w -e 's#href="contact-us.html"#href="/contact-us"#g;' *.html &&
+perl -pi -w -e 's#href="privacy.html"#href="/privacy"#g;' *.html &&
+sed -i '' '/Last Published/d' *.html &&
+sed -i '' '/This site was created in Webflow/d' *.html &&
+echo "Ancient chinese proverb."
