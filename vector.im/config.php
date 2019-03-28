d5b83e91e42a4674dccd06f7ce1a0b2a5db75d84
Nad Chishtie <n.chishtie@gmail.com>
2019-03-28 18:37:38 +0100

    Updated forwarding email.

--- vector.im/config.php
+++ vector.im/config.php
@@ -4,7 +4,7 @@ return array(
     // The name of the forwarding address inbox
     'forwarding_name' => 'New Vector Ltd Contact',
     // The email address to forward contact messages to
-    'forwarding_email' => 'nad@matrix.org',
+    'forwarding_email' => 'contact@vector.im',
     // Page to redirect to after sending mail successfully
     // Unset to disable redirection
     'success_redirect' => '/contact-us-success.html',
