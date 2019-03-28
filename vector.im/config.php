c6452e2ede7af2b4328f659eda05d89e95886332
Nad Chishtie <n.chishtie@gmail.com>
2019-03-28 12:33:11 +0100

    Changed test email

--- vector.im/config.php
+++ vector.im/config.php
@@ -4,7 +4,7 @@ return array(
     // The name of the forwarding address inbox
     'forwarding_name': 'New Vector Ltd Contact',
     // The email address to forward contact messages to
-    'forwarding_email': 'contact@vector.im',
+    'forwarding_email': 'nad@matrix.org',
 );
 
 ?>
