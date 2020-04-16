8d16710c8561e75573b3449433d9cfb1dea198a1
Nad Chishtie <n.chishtie@gmail.com>
2020-04-16 18:09:01 +0100

    Add runner.js for easier local testing

---
+++ runner.js
@@ -0,0 +1,28 @@
+/*
+* runner.js: start a new localhost http server on port 300.
+* makes it easy to see the generated site.
+*
+* to run: node runner.js
+*/
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+process.chdir(__dirname + "/vector.im");
+
+app.get('*', function (req, res) {
+    var path = process.cwd() + req.path;
+    if (fs.existsSync(path)) {
+        res.sendFile(path);
+    } else {
+        res.sendFile(path + '.html');
+    }
+});
+
+var server = app.listen(8989, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Listening at http://%s:%s', host, port);
+
+});
