bcf48526ffc6cad985b5f9992dc0f43972ac7913
Michael Telatynski <7t3chguy@gmail.com>
2020-04-18 17:46:39 +0100

    Improve webflow helper tooling to extract inline-src scripts and do more

--- webflow-helper.sh
+++ webflow-helper.sh
@@ -1,11 +1,34 @@
-cd vector.im &&
-perl -pi -w -e 's#href="index.html"#href="/"#g;' *.html &&
-perl -pi -w -e 's#href="careers.html"#href="/careers"#g;' *.html &&
-perl -pi -w -e 's#href="contact-us.html"#href="/contact-us"#g;' *.html &&
-perl -pi -w -e 's#href="privacy.html"#href="/privacy"#g;' *.html &&
-perl -pi -w -e 's#href="for-education.html"#href="/for-education"#g;' *.html &&
-perl -pi -w -e 's#href="for-healthcare.html"#href="/for-education"#g;' *.html &&
-perl -pi -w -e 's#href="for-crisis-response.html"#href="/for-education"#g;' *.html &&
-sed -i '' '/Last Published/d' *.html &&
-sed -i '' '/This site was created in Webflow/d' *.html &&
+#!/usr/bin/env bash
+
+# exit when any command fails
+set -e
+cd vector.im
+
+# replace wrong links
+perl -pi -w -e 's#href="index.html"#href="/"#g;' -- *.html
+perl -pi -w -e 's#href="careers.html"#href="/careers"#g;' -- *.html
+perl -pi -w -e 's#href="contact-us.html"#href="/contact-us"#g;' -- *.html
+perl -pi -w -e 's#href="privacy.html"#href="/privacy"#g;' -- *.html
+perl -pi -w -e 's#href="for-education.html"#href="/for-education"#g;' -- *.html
+perl -pi -w -e 's#href="for-healthcare.html"#href="/for-education"#g;' -- *.html
+perl -pi -w -e 's#href="for-crisis-response.html"#href="/for-education"#g;' -- *.html
+
+# remove webflow annotations
+sed -i '/Last Published/d' -- *.html
+sed -i '/This site was created in Webflow/d' -- *.html
+
+# replace jquery with a local one
+perl -pi -w -e 's@<script src=".*?js/jquery.+?</script>@<script src="/js/jquery-3.3.1.min.js" type="text/javascript"></script>@g;' -- *.html
+
+# extract inline javascript sections
+while read -r line
+do
+  hash=$(echo -n "$line" | md5sum | awk '{print $1}')
+  jsfile="js/webflow/$hash.js"
+  # write out js file
+  echo "$line" > "$jsfile"
+  # overwrite any matches in all files
+  sed -i "s@<script type=\"text/javascript\">$line</script>@<script src=\"$jsfile\" type=\"text/javascript\"></script>@g" -- *.html
+done < <(grep -hoP '(?<=<script type="text\/javascript">)(.+?)(?=<\/script>)' -- *.html | sort --unique)
+
 echo "Ancient chinese proverb."
